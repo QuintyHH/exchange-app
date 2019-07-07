5793af4f6555fca1152379753457ae41fd42c19d
QuintyHH <razmocanu@yahoo.com>
2019-07-07 05:19:49 +0300

    row colors onHover

--- src/components/history-list/history-list.style.js
+++ src/components/history-list/history-list.style.js
@@ -5,14 +5,28 @@ export const StyledHistoryList = styled.table`
     color:black;
     font-weight:bold;
   
+  header {
+    font-size: 20px;
+    color: #E4A444;
+    text-align: left;
+    margin-bottom: 10px;
+    padding-left: 5px;
+    border: 2px solid #E4A444
+  }
+
   tr:nth-child(odd) {
     background-color: #E4A444;
     color:white;
+    &:hover {
+      color:#59554F;
+    }
   }
   
   tr:nth-child(even) {
     background-color: #F5D4A3;
     color:#E4A444;
+    &:hover {
+      color:#59554F;
+    }
   }
-
 `

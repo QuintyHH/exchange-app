ed6801cac2e773de917e082eaada8fde95a8e3fc
QuintyHH <razmocanu@yahoo.com>
2019-07-12 19:29:51 +0300

    Updates

---
+++ precache-manifest.1d588b45bcb7c2b5d158c9782de192b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b70a0c41a17693e866e1d80f94cbd59f",
+    "url": "/exchange-app/index.html"
+  },
+  {
+    "revision": "00899f431afde7e0e41f",
+    "url": "/exchange-app/static/css/main.87ee6724.chunk.css"
+  },
+  {
+    "revision": "7905fbfbb4dfa030b5e8",
+    "url": "/exchange-app/static/js/2.82a08ac6.chunk.js"
+  },
+  {
+    "revision": "00899f431afde7e0e41f",
+    "url": "/exchange-app/static/js/main.7db58171.chunk.js"
+  },
+  {
+    "revision": "43d78a810dc729a1c474",
+    "url": "/exchange-app/static/js/runtime~main.80d10873.js"
+  }
+]);
